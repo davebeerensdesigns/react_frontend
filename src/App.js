483558054221a2c97057de8fd12ee2b323b8e02e
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-29 11:23:12 +0200

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import ListEmployeeComponent from "./components/list-employee.component";
+import HeaderComponent from "./components/header.component";
+import FooterComponent from "./components/footer.component";
 
 function App() {
-  return (
-    <div>
-      <ListEmployeeComponent />
-    </div>
-  );
+    return (
+        <div>
+            <HeaderComponent/>
+            <ListEmployeeComponent/>
+            <FooterComponent/>
+        </div>
+    );
 }
 
 export default App;
