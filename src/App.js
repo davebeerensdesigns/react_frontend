e273b3b3e80c692a99fedc64f19546e74f32c4fa
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-29 11:32:03 +0200

    create routes for employee

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ListEmployeeComponent from "./components/list-employee.component";
 import HeaderComponent from "./components/header.component";
 import FooterComponent from "./components/footer.component";
@@ -6,9 +7,16 @@ import FooterComponent from "./components/footer.component";
 function App() {
     return (
         <div>
-            <HeaderComponent/>
-            <ListEmployeeComponent/>
-            <FooterComponent/>
+            <BrowserRouter>
+                <HeaderComponent/>
+                <div className="container">
+                    <Routes>
+                        <Route path="/" exact element={<ListEmployeeComponent />}/>
+                        <Route path="/employees" element={<ListEmployeeComponent />}/>
+                    </Routes>
+                </div>
+                <FooterComponent/>
+            </BrowserRouter>
         </div>
     );
 }
