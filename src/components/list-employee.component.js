00088bca7360bad870b1d198bbecd7026862cc93
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-29 11:17:33 +0200

    created employee service to list employees in table

--- src/components/list-employee.component.js
+++ src/components/list-employee.component.js
@@ -1,26 +1,40 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import EmployeeService from "../services/employee.service";
 
 const ListEmployeeComponent = () => {
     const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        EmployeeService.getAllEmployees().then(response => {
+            setEmployees(response.data);
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }, []);
+
+
     return (
         <div className="container">
             <h2 className="text-center">List Employees</h2>
             <table className="table table-bordered table-striped">
                 <thead>
-                    <th>Employee Id</th>
-                    <th>Employee First Name</th>
-                    <th>Employee Last Name</th>
-                    <th>Employee Email</th>
+                    <tr>
+                        <th>Employee Id</th>
+                        <th>Employee First Name</th>
+                        <th>Employee Last Name</th>
+                        <th>Employee Email</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    {employees.map(employee => (
-                        <tr key={employee.id}>
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.emailId}</td>
-                        </tr>
-                    ))}
+                {employees.map(employee => (
+                    <tr key={employee.id}>
+                        <td>{employee.id}</td>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.emailId}</td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
         </div>
